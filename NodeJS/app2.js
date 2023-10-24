f6a3a375aa3443f5b53dec0063b0596c7415ac6f
calviem <calviem@rose-hulman.edu>
2023-10-24 14:08:25 -0400

    practice10

--- NodeJS/app2.js
+++ NodeJS/app2.js
@@ -8,3 +8,5 @@ const chalk = require('chalk');
 const success = chalk.green.inverse.bold('success');
 const failure = chalk.red.bold.inverse('failure');
 console.log(success, ' and ', failure);
+const additions = chalk.green.inverse.bold('something');
+console.log(additions);
