d56b9388868879eab73963c6dffdd8dc063ce65a
calviem <calviem@rose-hulman.edu>
2023-10-24 14:04:08 -0400

    practice 10

---
+++ NodeJS/PlayGround/challenge.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+
+const dataBuffer = fs.readFileSync('data.json');
+const dataJSON = dataBuffer.toString();
+const data = JSON.parse(dataJSON);
+console.log(data.name, data.city)
+data.name = "Matteo";
+data.city = "Terre Haute";
+const updateDataJSON = JSON.stringify(data)
+fs.writeFileSync('data.json', updateDataJSON)
---
+++ NodeJS/app2.js
@@ -0,0 +1,10 @@
+// const validator = require("validator");
+// console.log(validator.isEmail("olga@example.com"));
+// console.log(validator.isEmail("example.com"));
+// console.log(validator.isURL("example.com"));
+
+
+const chalk = require('chalk');
+const success = chalk.green.inverse.bold('success');
+const failure = chalk.red.bold.inverse('failure');
+console.log(success, ' and ', failure);
